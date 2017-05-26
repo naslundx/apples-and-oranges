46374c596c9841928c6354c9449c680d9e369564
Marcus Näslund <naslundx@gmail.com>
2017-05-26 22:58:59 +0200

    Working prototype, yay!

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -7,15 +7,17 @@ mongoose.connect('');
 // Schema for sets
 var SetModel = mongoose.model('sets', new mongoose.Schema({
     size: Number,
+    id: Number,
     title: String,
     items: [String]
 }));
 
 // Schema for results
 var ResultModel = mongoose.model('results', new mongoose.Schema({
-    first: Number,
-    second: Number,
-    value: String
+    firstId: Number,
+    secondId: Number,
+    firstIndices: [String],
+    secondIndices: [String]
 }));
 
 // URL parser
@@ -25,52 +27,45 @@ module.exports = function(app) {
 
 app.get('/main', function(req, res) {
     // Random set size
-    var size = 3; // Math.floor(Math.random() * (7 - 2 + 1)) + 2;
-    console.log('Fetching size = ' + size);
+    var size = 3; //Math.floor(Math.random() * 6) + 2;
 
     // Fetch all of certain size
     SetModel.find({'size': size}, function (err, data) {
         if (err) {
             throw err;
         }
-        var first = 0; //Math.floor(Math.random() * (data.length + 1));
-        var second = 1; // Math.floor(Math.random() * (data.length + 1)); // TODO make sure first != second
+        var first = Math.floor(Math.random() * data.length);
+        var second = (first + 1 + Math.floor(Math.random() * (data.length - 1))) % data.length;
         
-        console.log(first + ':' + data[first]);
-        console.log(second + ':' + data[second]);
+        //console.log(first + ':' + data[first]);
+        //console.log(second + ':' + data[second]);
 
-        res.render('main', {first: data[first], second: data[second]});
+        res.render('main', {firstId: data[first].id, secondId: data[second].id, first: data[first], second: data[second]});
     });
 });
 
 app.post('/main', urlencodedParser, function(req, res) {
     // Get the data client has sent and store as result
-    // TODO do some validation on req.body??
-    console.log(req.body);
     var newResultModel = ResultModel(req.body).save(function (err, data) {
         if (err) {
             throw err;
         }
 
-        res.redirect('thanks')
-        // TODO return some data about this set, or generally
-        // TODO render thank you page with option to load new
-    });
-});
+        console.log(req.body);
 
-// TODO can be removed?
-/*
-app.delete('/main/:item', function(req, res) {
-    
-    // Delete requested item from mongodb
-    
-    SetModel.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function (err, data) {
-        if (err) {
-            throw err;
-        }
-        res.json(data); 
+        var item = ResultModel({
+            firstId: req.body['firstId'],
+            secondId: req.body['secondId'],
+            firstIndices: req.body['firstIndices[]'],
+            secondIndices: req.body['secondIndices[]']
+        }).save(function(err) {
+            if (err) {
+                throw err;
+            }                
+        });
+
+        res.redirect('thanks'); // TODO return some data about this set, or generally
     });
 });
-*/
 
 };
--- public/client-to-server.js
+++ public/client-to-server.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
 
   $('form').on('submit', function(){
 
-      //var item = $('form input');
       var result = {
-        // TODO save correct values
-        first: 0,
-        second: 1,
-        value: "132231"
+        firstId: firstId,
+        secondId: secondId,
+        firstIndices: firstIndices,
+        secondIndices: secondIndices
       };
 
       $.ajax({
@@ -15,28 +14,11 @@ $(document).ready(function(){
         url: '/main',
         data: result,
         success: function(data){
-          // TODO how to get the new-rendered thanks page?
           window.location.href = 'thanks';
         }
       });
 
       return false;
-
   });
-
-/*
-// TODO can be removed?
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/main/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-  });
-*/
 
 });
